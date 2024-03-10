3a0dce0966a9043e62357226ab750b322dbd28a7
Khrizz <liveitstream@gmail.com>
2024-03-10 17:01:16 +0200

    Khrizz
    Fix json + refactoring

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+require_once 'Functions/FormatTitle.php';
+require_once 'Functions/GetLinkScraper.php';
+require_once 'Functions/ProcessArticle.php';
+
+$siteToScrape = 'https://www.hotnews.ro/sitemap-recent.xml';
+$links = getLinkScrape($siteToScrape);
+
+$output = [];
+foreach ($links as $index => $link) {
+    $article = processArticle($link['URL']);
+    $output[] = [
+        'No' => $index+1,
+        'URL' => $link['URL'],
+        'Date' => $article['date'],
+        'Body' => $article['body']
+    ];
+}
+
+$jsonOutput = json_encode($output, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+$file = 'output.json';
+file_put_contents($file, $jsonOutput);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,33 +31,9 @@
 </head>
 <body>
 <?php
-require_once 'Functions/FormatTitle.php';
-require_once 'Functions/GetLinkScraper.php';
-require_once 'Functions/ProcessContentDate.php';
-require_once 'Functions/ProcessContentArticleBody.php';
-
-$siteToScrape = 'https://www.hotnews.ro/sitemap-recent.xml';
-$links = getLinkScrape($siteToScrape);
-
-$output = "";
-foreach ($links as $index => $link) {
-    $date = processLinkContent($link['URL']);
-    $articleBodyContent = processContentArticleBody($link['URL']);
-    $articleBodyContent = htmlspecialchars($articleBodyContent, ENT_QUOTES | ENT_HTML5, 'UTF-8');
-
-    $output .= ($index + 1) . ": " . "<br>" . 
-               "<span>Title: </span>" . htmlspecialchars($link['Title'], ENT_QUOTES | ENT_HTML5, 'UTF-8') . "<br>" .
-               "<span>URL: </span>" . 
-               '<a href="' . $link['URL'] . '" target="_blank">' . $link['URL'] . '</a>' . "<br>" .
-               "<span>Date: </span>" . $date . "<br>" . 
-               "<span>Article Body:</span>" . $articleBodyContent . "<br><br>";
-}
-
-$jsonOutput = json_encode($output, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-$file = 'output.json';
-file_put_contents($file, $jsonOutput);
-
-echo $output;
+    echo '<pre>';
+    print_r($output);
+    echo '</pre>';
 ?>
 </body>
 </html>
